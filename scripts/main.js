6d69f605596bae811c0e7cd83fd2f10523663f1f
Jeanlouis90 <jeanlouisjabouin@gmail.com>
2018-04-08 15:10:21 -0400

    Second version. Finished up array education. Started on For loops.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,46 @@
 // Arrays
-var colors = ['red', ' blue', ' green'];
+// Push Exercise
+// var main = ['Hogan', ' Bret', ' Austin'];
 
-alert(colors[1]);
+// alert(main[0]);
 
-var colors = new Array('cyan',' magenta', ' yellow', ' black');
 
-colors.push('purple');
+// main.push(' Rock');
+// alert(main[3]);
+// main.push(' Lesnar');
+// alert(main[4]);
+// main.push(' Cena');
+// alert(main[5]);
+// main.push(' Lesnar');
+// alert(main[6]);
+// main.push(' Roman');
+// alert(main[7]);
+// alert(main);
 
-alert(colors[2]);
+// alert('length = ', main.length);
+
+// alert("rivals");
+
+// var rivals = ['Savage', ' Michaels', ' Rock'];
+// alert(rivals[0]);
+
+// rivals.push(' HHH');
+// alert(rivals[3]);
+// rivals.push(' HHH');
+// alert(rivals[4]);
+// rivals.push(' Orton');
+// alert(rivals[5]);
+// rivals.push(' Roman');
+// alert(rivals[6]);
+// rivals.push(' Strauman');
+// alert(rivals[7]);
+// alert(rivals);
+
+// alert('length = ', rivals.length);
+
+// var colors = new Array('cyan',' magenta', ' yellow', ' black');
+
+// FOR LOOPS
+console.log('HELLO');
+for (let i = 0; i < 10; i++) {
+}
